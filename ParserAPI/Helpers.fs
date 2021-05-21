b4989ec4b939739f148665d5f95626e11c885455
Maxumka <maxumka1792@gmail.com>
2021-05-21 14:27:28 -0400

    add helps function for string builder type and optional type

--- ParserAPI/Helpers.fs
+++ ParserAPI/Helpers.fs
@@ -1,6 +1,7 @@
 ﻿namespace ParserAPI
 
 open System
+open System.Text
 
 module HelperTryParse = 
     let TryParseIntOption(value: string) = 
@@ -12,3 +13,19 @@ module HelperTryParse =
         match (Double.TryParse value) with 
         | true, res -> Some res
         | _, _ -> None
+
+module StringBuilderHelpers = 
+    let out x = x.ToString()
+
+    let (++) (left : StringBuilder) (right : 't option) = 
+        match right with 
+        | Some value -> left.Append value 
+        | None -> left 
+
+    let (+=) (left : StringBuilder) (right : 't option) = left ++ right |> ignore
+
+module OptionHelpers = 
+    let execFun fn (x : 't option) = 
+        match x with 
+        | Some value -> Some <| fn value
+        | None -> None 
