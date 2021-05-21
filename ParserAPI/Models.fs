8324f6d53486c688f13b2345dae549a8e07ea1bc
Maxumka <maxumka1792@gmail.com>
2021-05-21 14:26:58 -0400

    edit conditions type in parser

--- ParserAPI/Models.fs
+++ ParserAPI/Models.fs
@@ -5,27 +5,37 @@ open System
 
 module Models = 
 
-    type Conditions = { fromMileage : int option 
-                        toMileage   : int option
-                        fromPower   : int option
-                        toPower     : int option
-                        fromVolume  : double option
-                        toVolume    : double option
-                        fromYear    : int option
-                        toYear      : int option
-                        fromPrice   : int option
-                        toPrice     : int option}
+    type Conditions = { fromMileage   : int option 
+                        toMileage     : int option
+                        fromPower     : int option
+                        toPower       : int option
+                        fromVolume    : double option
+                        toVolume      : double option
+                        fromYear      : int option
+                        toYear        : int option
+                        fromPrice     : int option
+                        toPrice       : int option
+                        tranmission   : Transmission
+                        engine        : Engine
+                        gear          : Gear 
+                        steeringWheel : SteeringWheel
+                        carBody       : CarBody}
                         with static member FromCarFromModel (car : CarFormModel) = 
-                                    { fromMileage = Option.ofNullable car.FromMileage
-                                      toMileage   = Option.ofNullable car.ToMileage
-                                      fromPower   = Option.ofNullable car.FromEnginePower
-                                      toPower     = Option.ofNullable car.ToEnginePower
-                                      fromVolume  = Option.ofNullable car.FromEngineVolume
-                                      toVolume    = Option.ofNullable car.ToEngineVolume
-                                      fromYear    = Option.ofNullable car.FromYear
-                                      toYear      = Option.ofNullable car.ToYear
-                                      fromPrice   = Option.ofNullable car.FromPrice
-                                      toPrice     = Option.ofNullable car.ToPrice}
+                                    { fromMileage   = Option.ofNullable car.FromMileage
+                                      toMileage     = Option.ofNullable car.ToMileage
+                                      fromPower     = Option.ofNullable car.FromEnginePower
+                                      toPower       = Option.ofNullable car.ToEnginePower
+                                      fromVolume    = Option.ofNullable car.FromEngineVolume
+                                      toVolume      = Option.ofNullable car.ToEngineVolume
+                                      fromYear      = Option.ofNullable car.FromYear
+                                      toYear        = Option.ofNullable car.ToYear
+                                      fromPrice     = Option.ofNullable car.FromPrice
+                                      toPrice       = Option.ofNullable car.ToPrice
+                                      tranmission   = car.Transmission
+                                      engine        = car.Engine
+                                      gear          = car.Gear 
+                                      steeringWheel = car.SteeringWheel
+                                      carBody       = car.CarBody}
                                 
                             
 
